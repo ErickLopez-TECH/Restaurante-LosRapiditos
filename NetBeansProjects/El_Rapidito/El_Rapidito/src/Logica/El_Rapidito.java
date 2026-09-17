21cd7f607660152d885e266a33012f2a23dbc6fa
Erick <ericklopezborge21@gmail.com>
2026-09-17 17:59:31 -0600

    Login Listo

---
+++ NetBeansProjects/El_Rapidito/El_Rapidito/src/Logica/El_Rapidito.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Logica;
+import Presentacion.FrmLogin;
+/**
+ *
+ * @author triamus
+ */
+public class El_Rapidito {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FrmLogin lgn = new FrmLogin();
+        lgn.setLocationRelativeTo(null);//Establezca la posicion de acuerdo a nada
+        lgn.setVisible(true);
+    }
+    
+}
